f1718786af7574b6e6cd13c1935ed7b710ef9f5e
Jatinkumar007 <jatin.kr9868@gmail.com>
2023-01-12 01:29:35 +0530

    error solved

--- primeproblem.js
+++ primeproblem.js
@@ -1,11 +1,8 @@
 let n=5;
 let count=0;
+for(let i=0;i<n;i++){
 if(n%i==0){
     count++;
 }
-if(==0){
-    console.log("number is prime");
 }
-else{
-    console.log("number is not prime");
-}
+(n==2) ? console.log(n,"is prime") : console.log(n,"is not prime")
